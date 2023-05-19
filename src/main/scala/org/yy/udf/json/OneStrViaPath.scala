8bc8c9b66a2a48788d8788bbf48c4311e248d7c3
yangyongyong <yangyong2143@gmail.com>
2023-05-20 01:40:04 +0800

    update

--- src/main/scala/org/yy/udf/json/OneStrViaPath.scala
+++ src/main/scala/org/yy/udf/json/OneStrViaPath.scala
@@ -2,7 +2,6 @@ package org.yy.udf.json
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{expr, udf}
-import com.alibaba.fastjson2.JSON
 import com.alibaba.fastjson2.{JSONPath, JSONReader}
 
 import scala.util.Try
@@ -13,10 +12,10 @@ object OneStrViaPath {
         jsonArray/jsonObject都可 String类型
         jsonpath String类型
     输出:
-        类型: array[String]
+        类型: String
     使用方法:
-      spark.udf.register("arrayonestr_from_json_via_path",arrayonestr_from_json_via_path)
-    注意: jsonpath解析不出来 或者列值为null 都返回空数组
+        spark.udf.register("onestr_from_json_via_path",oneonestr_from_json_via_path)
+    注意: jsonpath解析不出来 或者列值为null 都返回 null
      */
     val oneonestr_from_json_via_path = udf(
         (js: String, jsPath: String) => {
@@ -54,6 +53,17 @@ object OneStrViaPath {
 
 
         spark.sql("""select onestr_from_json_via_path('[{"des":"i am 1","id":1},{"des":"i am 2","id":2},{"des":"i am 0","id":0}]','$[?(@.id = 0)].des[1]') as v """)
+          .withColumn("typename", expr("typeof(v)"))
+          .show(false)
+        /*
+        +----+--------+
+        |v   |typename|
+        +----+--------+
+        |null|string  |
+        +----+--------+
+         */
+
+        spark.sql("""select onestr_from_json_via_path(null,'$[?(@.id = 0)].des[1]') as v """)
           .withColumn("typename", expr("typeof(v)"))
           .show(false)
         /*
